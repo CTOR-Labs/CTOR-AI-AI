f2ac30ebda6adf18a4eaef518ae5e6b4cef872b7
CTOR‑Federation. <bva5@hotmail.com>
2026-02-10 15:22:05 -0500

    Update 04_aggressiveBot.js

--- examples/04_aggressiveBot.js
+++ examples/04_aggressiveBot.js
@@ -1,19 +1,50 @@
+// 04_aggressiveBot.js
+// Strategy: prefer moves whose target cell is adjacent (orthogonally) to enemy pieces.
+
 function bot(board, player) {
-  const enemy = player === "R" ? "B" : "R";
-  const moves = [];
+  const moves = game.getLegalMoves(player);
+  if (moves.length === 0) return null;
 
-  for (let i = 0; i < 10; i++)
-    for (let j = 0; j < 10; j++)
-      if (board[i][j] === ".") {
-        const neigh =
-          (board[(i+1)%10][j] === enemy) +
-          (board[(i+9)%10][j] === enemy) +
-          (board[i][(j+1)%10] === enemy) +
-          (board[i][(j+9)%10] === enemy);
+  const N = board.length;
+  const enemy = (player === "R") ? "B" : "R";
 
-        moves.push({ move: { type:"put", i, j }, score: neigh });
-      }
+  function countEnemyOrthNeighbors(i, j) {
+    let c = 0;
+    const up    = (i - 1 + N) % N;
+    const down  = (i + 1) % N;
+    const left  = (j - 1 + N) % N;
+    const right = (j + 1) % N;
 
-  moves.sort((a,b)=>b.score-a.score);
-  return moves[0].move;
+    if (board[up][j] === enemy) c++;
+    if (board[down][j] === enemy) c++;
+    if (board[i][left] === enemy) c++;
+    if (board[i][right] === enemy) c++;
+    return c;
+  }
+
+  let best = null;
+  let bestScore = -Infinity;
+
+  for (const m of moves) {
+    let ti, tj;
+
+    if (m.type === "put") {
+      ti = m.i;
+      tj = m.j;
+    } else if (m.type === "move") {
+      [ti, tj] = m.to;
+    } else {
+      continue;
+    }
+
+    const score = countEnemyOrthNeighbors(ti, tj);
+
+    if (score > bestScore) {
+      bestScore = score;
+      best = m;
+    }
+  }
+
+  return best || moves[0];
 }
+
